5c1d652f16e2ff4c498dab5cfec5b777bdb41496
imronpuji <imronpuji5@gmail.com>
2021-07-31 19:10:29 +0700

    'text'

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
 export default axios.create({
-  baseURL: 'https://akutansi321.herokuapp.com/api'
+  baseURL: 'https://d75d59946237.ngrok.io/api'
   // baseURL: 'https://1571156cb4fc.ngrok.io'
 
 })
