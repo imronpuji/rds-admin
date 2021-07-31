5f25e88abd8b3445aeca9a8b123278f00fff9b78
imronpuji <imronpuji5@gmail.com>
2021-07-31 14:47:04 +0700

    fixapi

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
 export default axios.create({
-  baseURL: 'http://c20935464ba8.ngrok.io/api'
+  baseURL: 'https://akutansi321.herokuapp.com/api'
   // baseURL: 'https://1571156cb4fc.ngrok.io'
 
 })
