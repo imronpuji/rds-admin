4adb4fa614b488f75c8a058891ca9b6020b4072b
imronpuji <imronpuji5@gmail.com>
2021-08-08 01:05:02 +0700

    finalOne

--- src/views/akun.vue
+++ src/views/akun.vue
@@ -1,225 +1,249 @@
 <template>
-<div class="app-container">
-    <div class="filter-container">
-
-        <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
-            Add
-        </el-button>
-    </div>
-    <el-tree :data="list" :props="defaultProps">
-        <span class="custom-tree-node" slot-scope="{ node, data }">
-            <span>{{ node.label }}</span>
-            <span style="margin-left:20px">
-                <el-button type="text" size="mini" @click="() => handleDelete(data, data)">
-                    Delete
-                </el-button>
-                <el-button type="text" size="mini" @click="() => handleUpdate(data, data)">
-                    Edit
-                </el-button>
-            </span>
-        </span>
-    </el-tree>
-
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-        <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-            <el-form-item label="Kategori" props="category">
-                <el-select v-model="category" required class="filter-item" placeholder="Please select" @change="onChangeModal($event)">
-                    <el-option label="Kosong" value="" />
-                    <el-option v-for="item in categories" :key="item.id" :label="item.name" :value="item.id" />
-                </el-select>
-            </el-form-item>
-            <el-form-item label="Nama">
-                <el-input placeholder="Nama" v-model="name" />
-            </el-form-item>
-            <el-form-item v-if="header != true">
-                <el-checkbox v-model="kas">Bank / Kas</el-checkbox>
-            </el-form-item>
-            <el-form-item v-if="kas != true">
-                <el-checkbox v-model="header">Header</el-checkbox>
-            </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="dialogFormVisible = false">
-                Cancel
-            </el-button>
-            <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
-                Confirm
+    <div class="app-container">
+        <div class="filter-container">
+            <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
+                Add
             </el-button>
         </div>
-    </el-dialog>
+        <el-tree :data="list" :props="defaultProps">
+            <span class="custom-tree-node" slot-scope="{ node, data }">
+                <span>{{ node.label }}</span>
+                <span style="margin-left:20px">
+                    <el-button type="text" size="mini" @click="() => handleDelete(data, data)">
+                        Delete
+                    </el-button>
+                    <el-button type="text" size="mini" @click="() => handleUpdate(data, data)">
+                        Edit
+                    </el-button>
+                </span>
+            </span>
+        </el-tree>
 
-    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
-        <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
-            <el-table-column prop="key" label="Channel" />
-            <el-table-column prop="pv" label="Pv" />
-        </el-table>
-        <span slot="footer" class="dialog-footer">
-            <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
-        </span>
-    </el-dialog>
-</div>
+        <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+            <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
+                <el-form-item label="Kategori" props="category">
+                    <el-select v-model="category" required class="filter-item" placeholder="Please select" @change="onChangeModal($event)">
+                        <el-option label="Kosong" value="" />
+                        <el-option v-for="item in categories" :key="item.id" :label="item.name" :value="item.id" />
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="Nama">
+                    <el-input placeholder="Nama" v-model="name" />
+                </el-form-item>
+                <el-form-item v-if="header != true">
+                    <el-checkbox v-model="kas">Bank / Kas</el-checkbox>
+                </el-form-item>
+                <el-form-item v-if="kas != true">
+                    <el-checkbox v-model="header">Header</el-checkbox>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">
+                    Cancel
+                </el-button>
+                <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
+                    Confirm
+                </el-button>
+            </div>
+        </el-dialog>
+
+        <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
+            <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
+                <el-table-column prop="key" label="Channel" />
+                <el-table-column prop="pv" label="Pv" />
+            </el-table>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
+            </span>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
-import {
-    fetchList,
-    fetchPv,
-    createArticle,
-    updateArticle
-} from '@/api/article'
-import waves from '@/directive/waves' // waves directive
-import {
-    parseTime
-} from '@/utils'
-import Pagination from '@/components/Pagination' // secondary package based on el-pagination
-import axios from '@/api/axios'
-import qs from 'qs'
-
-const calendarTypeOptions = [{
-        key: 'cash',
-        display_name: 'cash'
-    },
+    import 
     {
-        key: 'modal',
-        display_name: 'modal'
-    }
-]
+        fetchList,
+        fetchPv,
+        createArticle,
+        updateArticle
+    }   from '@/api/article'
 
-// arr to obj, such as { CN : "China", US : "USA" }
-const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
-    acc[cur.key] = cur.display_name
-    return acc
-}, {})
+    import waves from '@/directive/waves' // waves directive
+    import 
+    {
+        parseTime
+    } from '@/utils'
+    import Pagination from '@/components/Pagination' // secondary package based on el-pagination
+    import axios from '@/api/axios'
+    import qs from 'qs'
 
-export default {
-    name: 'ComplexTable',
-    components: {
-        Pagination
-    },
-    directives: {
-        waves
-    },
-    filters: {
-        statusFilter(status) {
-            const statusMap = {
-                published: 'success',
-                draft: 'info',
-                deleted: 'danger'
-            }
-            return statusMap[status]
+    const calendarTypeOptions = []
+
+    const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
+        acc[cur.key] = cur.display_name
+        return acc
+    }, {})
+
+    export default {
+        name: 'ComplexTable',
+        components: 
+        {
+            Pagination
         },
-        typeFilter(type) {
-            return calendarTypeKeyValue[type]
-        }
-    },
-    data() {
-        return {
-            defaultProps: {
-                children: 'children',
-                label: 'name'
-            },
-            id: '',
-            category: '',
-            header: '',
-            fullscreenLoading: false,
-            name: '',
-            desc: '',
-            tableKey: 0,
-            list: null,
-            total: 0,
-            kas: '',
-            listLoading: true,
-            listQuery: {
-                page: 1,
-                limit: 20,
-                importance: undefined,
-                title: undefined,
-                type: undefined,
-                sort: '+id'
-            },
-            importanceOptions: [1, 2, 3],
-            calendarTypeOptions,
-            categories: [],
-            modal: [],
-            sortOptions: [{
-                label: 'ID Ascending',
-                key: '+id'
-            }, {
-                label: 'ID Descending',
-                key: '-id'
-            }],
-            statusOptions: ['published', 'draft', 'deleted'],
-            showReviewer: false,
-            temp: {
-                id: undefined,
-                code: '',
-                date: '',
-                timestamp: new Date(),
-                title: '',
-                to: '',
-                chasin: '',
-                total: ''
-            },
-            dialogFormVisible: false,
-            dialogStatus: '',
-            textMap: {
-                update: 'Edit',
-                create: 'Create'
-            },
-            dialogPvVisible: false,
-            pvData: [],
-            rules: {
-                type: [{
-                    required: true,
-                    message: 'type is required',
-                    trigger: 'change'
-                }],
+        
+        directives: 
+        {
+            waves
+        },
+        
+        filters: 
+        {
+            statusFilter(status) 
+            {
+                const statusMap = 
+                {
+                    published: 'success',
+                    draft: 'info',
+                    deleted: 'danger'
+                }
+                return statusMap[status]
+                },
+            typeFilter(type) 
+            {
+                return calendarTypeKeyValue[type]
+            }
+        },
+        
+        data() {
+            return {
+                defaultProps: 
+                {
+                    children: 'children',
+                    label: 'name'
+                },
+                
+                id: '',
+                category: '',
+                header: '',
+                fullscreenLoading: false,
+                name: '',
+                desc: '',
+                tableKey: 0,
+                list: null,
+                total: 0,
+                kas: '',
+                listLoading: true,
+                
+                listQuery: 
+                {
+                    page: 1,
+                    limit: 20,
+                    importance: undefined,
+                    title: undefined,
+                    type: undefined,
+                    sort: '+id'
+                },
+                importanceOptions: [1, 2, 3],
+                calendarTypeOptions,
+                categories: [],
+                modal: [],
+
+                sortOptions: 
+                [
+                    {
+                        label: 'ID Ascending',
+                        key: '+id'
+                    }, 
+                    {
+                        label: 'ID Descending',
+                        key: '-id'
+                    }
+                ],
+                
+                statusOptions: ['published', 'draft', 'deleted'],
+                showReviewer: false,
+                temp: 
+                {
+                    id: undefined,
+                    code: '',
+                    date: '',
+                    timestamp: new Date(),
+                    title: '',
+                    to: '',
+                    chasin: '',
+                    total: ''
+                },
+                dialogFormVisible: false,
+                dialogStatus: '',
+                
+                textMap: 
+                {
+                    update: 'Edit',
+                    create: 'Create'
+                },
+
+                dialogPvVisible: false,
+                pvData: [],
+                
+                rules: 
+                {
+                    type: 
+                    [
+                        {
+                            required: true,
+                            message: 'type is required',
+                            trigger: 'change'
+                        }
+                    ],
                 // timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
-                title: [{
-                    required: true,
-                    message: 'title is required',
-                    trigger: 'blur'
-                }]
+                title: 
+                [
+                    {
+                        required: true,
+                        message: 'title is required',
+                        trigger: 'blur'
+                    }
+                ]
             },
             downloadLoading: false
         }
     },
+
     created() {
         this.getList()
     },
+
     methods: {
+        
         load(tree, treeNode, resolve) {
             setTimeout(() => {
                 resolve(this.list)
             }, 1000)
         },
-        getList() {
-            this.listLoading = true
 
-            axios.get('/akun').then(response => {
-                console.log(response)
+        getList() {
+
+            this.listLoading = true
+            axios.get('/akun').then(response => 
+            {
                 this.list = response.data.akun
                 this.total = response.data.akun.length
-
-                // Just to simulate the time of the request
-                setTimeout(() => {
-                    this.listLoading = false
-                }, 1.5 * 1000)
+                this.listLoading = false
             })
 
-            axios.get('/akun/isheader').then(response => {
+            axios.get('/akun/isheader').then(response => 
+            {
                 this.categories = response.data.akun
-                // Just to simulate the time of the request
-                setTimeout(() => {
-                    this.listLoading = false
-                }, 1.5 * 1000)
+                this.listLoading = false
             })
 
         },
+
         handleFilter() {
             this.listQuery.page = 1
             this.getList()
         },
+
         handleModifyStatus(row, status) {
             this.$message({
                 message: '操作Success',
@@ -270,12 +294,13 @@ export default {
                 iscash: this.kas == '' ? false : this.kas,
                 isheader: this.header == '' ? false : this.header
             }
-            console.log(data);
+
             this.listLoading = true;
             this.dialogFormVisible = false
 
             axios.post('/akun/create', data)
-                .then((response) => {
+                .then((response) => 
+                {
                     this.getList()
                     this.dialogFormVisible = false
                     this.$notify({
@@ -302,23 +327,6 @@ export default {
             })
         },
         updateData() {
-            // this.$refs['dataForm'].validate((valid) => {
-            //   if (valid) {
-            //     const tempData = Object.assign({}, this.temp)
-            //     tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
-            //     updateArticle(tempData).then(() => {
-            //       const index = this.list.findIndex(v => v.id === this.temp.id)
-            //       this.list.splice(index, 1, this.temp)
-            //       this.dialogFormVisible = false
-            //       this.$notify({
-            //         title: 'Success',
-            //         message: 'Update Successfully',
-            //         type: 'success',
-            //         duration: 2000
-            //       })
-            //     })
-            //   }
-            // })
 
             this.dialogFormVisible = false
             this.listLoading = true
