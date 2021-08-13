34d4564d8b76e16f52ddae2ad997a6fc717b16b1
imronpuji <imronpuji5@gmail.com>
2021-08-13 14:09:23 +0700

    'finalOne'

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
 export default axios.create({
-  baseURL: 'https://eko.kawanmama.com/api'
+  baseURL: 'https://akutansi456.herokuapp.com/api'
   // baseURL: 'https://1571156cb4fc.ngrok.io'
 
 })
