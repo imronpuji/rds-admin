99952a9771f664da0d39a25a2b662caa2cf1b86a
imronpuji <imronpuji5@gmail.com>
2021-08-15 00:02:44 +0700

    'lkl'

--- src/views/produk.vue
+++ src/views/produk.vue
@@ -79,10 +79,7 @@
       </el-table-column>
       <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
-           <el-button type="primary" size="mini" @click="handleDelete(row, $index)">
-            Delete
-          </el-button>
-          <el-button size="mini" type="danger" @click="handleUpdate(row)">
+          <el-button size="mini" type="primary" @click="handleUpdate(row)">
             Edit
           </el-button>
         </template>
