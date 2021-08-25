70b58e199af00f7550a44eef289929c6fa176b17
imronpuji <imronpuji5@gmail.com>
2021-08-25 20:13:05 +0700

    'test'

--- src/views/stok_keluar.vue
+++ src/views/stok_keluar.vue
@@ -1,34 +1,18 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">
-        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-      </el-select>
-      <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px">
-        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
-      </el-select>
-      <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
-      </el-select>
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-        Search
-      </el-button>
       <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
         Add
       </el-button>
       <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
         Export
       </el-button>
-      <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
-        reviewer
-      </el-checkbox>
     </div>
 
     <el-table
     :key="tableKey"
     v-loading="listLoading"
-    :data="list"
+    :data="list.filter(({contact}) => !search || contact.name.toLowerCase().includes(search.toLowerCase()))"
     border
     fit
     highlight-current-row
@@ -40,7 +24,13 @@
         <span>{{ row.id }}</span>
       </template>
     </el-table-column>
-    <el-table-column label="Customer" min-width="150px" sortable>
+    <el-table-column label="Customer" min-width="150px">
+       <template slot="header" slot-scope="scope">
+        <el-input
+        v-model="search"
+          size="mini"
+          placeholder="Type to search"/>
+      </template>
       <template slot-scope="{row}">
         <span v-if="row.contact != null" class="link-type" @click="handleUpdate(row)">{{ row.contact.name }}</span>
       </template>
@@ -200,13 +190,27 @@ export default {
       'name',
       'avatar',
       'roles'
-    ])
+    ]),
+
+
+
   },
+
+  // watch: {
+  //   // whenever question changes, this function will run
+  //   search: function (val) {
+  //     this.search = val
+  //     this.list =  this.list.filter(data => {
+  //       return data.contact.name.toLowerCase().includes(this.search.toLowerCase())
+  //     }) 
+  //   }
+  // },
   data() {
     return {
       category : '',
       kontak : [],
       kas :[],
+      search : '',
       product :[],
       contact_id :"",
       cashout_id:"",
@@ -306,6 +310,7 @@ export default {
        this.product = response.data.product
      })
     },
+
     handleCurrency(number){
      const idr = new Intl.NumberFormat('in-IN', { style: 'currency', currency: 'IDR' }).format(number)
      return idr
