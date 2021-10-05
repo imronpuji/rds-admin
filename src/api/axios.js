578085fab69bd52f2d4185f22829e0f14173884f
imronpuji <imronpuji5@gmail.com>
2021-10-05 22:41:05 +0700

    imron

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 // 'https://cors-anywhere.herokuapp.com/apisiakad.hilmimubarok.com'
 export default axios.create({
-  // baseURL: 'https://eko.kawanmama.com/api'
+  baseURL: 'https://eko.kawanmama.com/api'
   // baseURL: 'https://1571156cb4fc.ngrok.io'
-   baseURL: 'https://akutansi2021.kawanmama.com/public/api'
+   // baseURL: 'https://akutansi456.herokuapp.com/api'
 
 
 })
